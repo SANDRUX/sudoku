4477449e0b09cf718930055dff7ac2cd50874e57
SpookyPinkyTux <sandrotutberidze18@gmail.com>
2022-04-04 21:01:19 +0400

    game scene in progress

--- sudoku.hpp
+++ sudoku.hpp
@@ -7,5 +7,6 @@
 
 void draw_menu_scene(int, Rectangle, const char *, const bool);
 void update_menu_scene(Rectangle, bool, char *, int, int &);
-void game_scene();
+void draw_game_scene();
+void update_game_scene();
 void game_over_scene();
